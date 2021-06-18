7e559ced230676c04d3ab0e502705e85b838af5c
sheel1206 <sheelpatel2001@gmail.com>
2021-06-18 10:09:27 +0530

    Final Commit

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ import keras
 from keras.preprocessing import text,sequence
 import dill
 
-X = np.array([0])
 voc_size = 10000
+news_title = ""
 sent_length= 50
 model = keras.models.load_model('model/model.h5')
 
@@ -51,16 +51,15 @@ if nav == "HOME":
 elif nav == "CHECK YOUR NEWS":
     st.header("CHECK YOUR NEWS HERE!")
     news_title = st.text_area('Enter your news title below')
-    X = list()
-    X.append(news_title)
-def predict():
-    tokenizer = open('model/tokenizer.pkl', 'rb')
-    tokenized = joblib.load(tokenizer)
-    max_len = 300
-    tokenized_pred = tokenized.texts_to_sequences(X)
-    X = sequence.pad_sequences(tokenized_pred, maxlen=max_len)
 
-predict()
+X = list()
+X.append(news_title)
+tokenizer = open('model/tokenizer.pkl', 'rb')
+tokenized = joblib.load(tokenizer)
+max_len = 300
+tokenized_pred = tokenized.texts_to_sequences(X)
+X = sequence.pad_sequences(tokenized_pred, maxlen=max_len)
+
 prediction = model.predict_classes(X)
 
     
